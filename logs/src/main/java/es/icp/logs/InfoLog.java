febdb4b37f8614d87cdf1cd10c0c4711300b9d79
dgcodes-app <52632406+dgcodes-app@users.noreply.github.com>
2020-01-13 10:33:19 +0100

    Guardar el log en fichero

---
+++ logs/src/main/java/es/icp/logs/InfoLog.java
@@ -0,0 +1,71 @@
+package es.icp.logs;
+
+import java.io.BufferedReader;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class InfoLog {
+
+    public enum TIPO {TEXTO, PULSACION}
+
+    TIPO tipo;
+    String tiempo;
+    String activity;
+    String nombre;
+    String clase;
+    String duracion;
+    String extra;
+
+    public InfoLog(TIPO tipo, String tiempo, String activity, String nombre, String clase, String duracion, String extra) {
+        this.tipo = tipo;
+        this.tiempo = tiempo;
+        this.activity = activity;
+        this.nombre = nombre;
+        this.clase = clase;
+        this.duracion = duracion;
+        this.extra = extra;
+    }
+
+    public InfoLog(String cadena) {
+        this.tipo = TIPO.TEXTO;
+        long yourmilliseconds = System.currentTimeMillis();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        this.tiempo = sdf.format(new Date(yourmilliseconds));
+
+        this.extra = cadena;
+    }
+
+    @Override
+    public String toString() {
+        return "Info{" +
+                "tiempo='" + tiempo + '\'' +
+                ", activity='" + activity + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", clase='" + clase + '\'' +
+                ", duracion='" + duracion + '\'' +
+                ", extra='" + extra + '\'' +
+                '}';
+    }
+
+
+    public String toLog() {
+
+        switch (tipo) {
+            case TEXTO:
+                return tiempo + " => " +extra;
+
+
+            case PULSACION:
+                return tiempo + " => " +
+                        " activity='" + activity + '\'' +
+                        ", nombre='" + nombre + '\'' +
+                        ", clase='" + clase + '\'' +
+                        ", duracion='" + duracion + '\'' +
+                        ", extra='" + extra + '\'';
+            default:
+                return "DESCONOCIDO";
+        }
+
+    }
+
+}
--- logs/src/main/java/es/icp/logs/MyLog.java
+++ logs/src/main/java/es/icp/logs/MyLog.java
@@ -1,102 +1,171 @@
 package es.icp.logs;
 
 
+import android.content.Context;
 import android.util.Log;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 public class MyLog {
 
-    private static Boolean DEBUG = BuildConfig.DEBUG;
+    enum TIPO {DEBUG, ERROR, WARNING, INFORMATION, VERBOSE, INFO}
+
+   private static Boolean DEBUG = BuildConfig.DEBUG;
 
     /**
      * Etiqueta a mostrar en el log
      */
-    private static String TAG             = "DEBUG_ICP";
-    private static int    TABULACION_JSON = 4;
+    private static String TAG = "DEBUG_ICP";
+    private static int TABULACION_JSON = 4;
 
     //variables para configurar lo que queremos mostrar en el debug
     private static Boolean mostrarFichero = false;
-    private static Boolean mostrarClase   = true;
-    private static Boolean mostrarMetodo  = true;
-    private static Boolean mostrarLineas  = true;
+    private static Boolean mostrarClase = true;
+    private static Boolean mostrarMetodo = true;
+    private static Boolean mostrarLineas = true;
+
+    private static Boolean SAVE_FILE = true;
+    private static String CARPETA_LOG = "LOGS";
+    private static Context context;
+    private static StackTraceElement traceElement; //vb para almacenar la traza cuando la mostamos en el log, para poder guardarla en el ficheo si queremos.
 
 
-    public static Boolean getDEBUG (){ return DEBUG; }
+    public static Boolean getDEBUG() {
+        return DEBUG;
+    }
 
-    public static void setDEBUG (Boolean DEBUG){ MyLog.DEBUG = DEBUG; }
+    public static void setDEBUG(Boolean DEBUG) {
+        MyLog.DEBUG = DEBUG;
+    }
 
-    public static String getTAG (){ return TAG; }
+    public static String getTAG() {
+        return TAG;
+    }
 
-    public static void setTAG (String TAG){ MyLog.TAG = TAG; }
+    public static void setTAG(String TAG) {
+        MyLog.TAG = TAG;
+    }
 
-    public static int getTabulacionJson (){ return TABULACION_JSON; }
+    public static int getTabulacionJson() {
+        return TABULACION_JSON;
+    }
 
-    public static void setTabulacionJson (int tabulacionJson){ TABULACION_JSON = tabulacionJson; }
+    public static void setTabulacionJson(int tabulacionJson) {
+        TABULACION_JSON = tabulacionJson;
+    }
 
-    public static Boolean getMostrarFichero (){ return mostrarFichero; }
+    public static Boolean getMostrarFichero() {
+        return mostrarFichero;
+    }
 
-    public static void setMostrarFichero (Boolean mostrarFichero){ MyLog.mostrarFichero = mostrarFichero; }
+    public static void setMostrarFichero(Boolean mostrarFichero) {
+        MyLog.mostrarFichero = mostrarFichero;
+    }
 
-    public static Boolean getMostrarClase (){ return mostrarClase; }
+    public static Boolean getMostrarClase() {
+        return mostrarClase;
+    }
 
-    public static void setMostrarClase (Boolean mostrarClase){ MyLog.mostrarClase = mostrarClase; }
+    public static void setMostrarClase(Boolean mostrarClase) {
+        MyLog.mostrarClase = mostrarClase;
+    }
 
-    public static Boolean getMostrarMetodo (){ return mostrarMetodo; }
+    public static Boolean getMostrarMetodo() {
+        return mostrarMetodo;
+    }
 
-    public static void setMostrarMetodo (Boolean mostrarMetodo){ MyLog.mostrarMetodo = mostrarMetodo; }
+    public static void setMostrarMetodo(Boolean mostrarMetodo) {
+        MyLog.mostrarMetodo = mostrarMetodo;
+    }
 
-    public static Boolean getMostrarLineas (){ return mostrarLineas; }
+    public static Boolean getMostrarLineas() {
+        return mostrarLineas;
+    }
 
-    public static void setMostrarLineas (Boolean mostrarLineas){ MyLog.mostrarLineas = mostrarLineas; }
+    public static void setMostrarLineas(Boolean mostrarLineas) {
+        MyLog.mostrarLineas = mostrarLineas;
+    }
+
+    public static void init(Context ctx) {
+        context = ctx;
+    }
 
     /**
      * Mensaje de DEBUG
      *
      * @param texto Texto a mostrar
      */
-    public static void d (Object texto){ if (DEBUG) Log.d(TAG, construirTexto(texto.toString(), 5)); }
+    public static void d(Object texto) {
+        String cadena = construirTexto(texto.toString(), 5);
+        if (DEBUG) Log.d(TAG, cadena);
+        if (SAVE_FILE) saveFichero(TIPO.DEBUG, cadena);
+    }
 
     /**
      * Mensaje de ERROR
      *
      * @param texto Texto a mostrar
      */
-    public static void e (Object texto){ if (DEBUG) Log.e(TAG, construirTexto(texto.toString(), 5)); }
+    public static void e(Object texto) {
+        String cadena = construirTexto(texto.toString(), 5);
+        if (DEBUG) Log.e(TAG, cadena);
+        if (SAVE_FILE) saveFichero(TIPO.ERROR, cadena);
+    }
 
     /**
      * Mensaje de WARNING
      *
      * @param texto Texto a mostrar
      */
-    public static void w (Object texto){ if (DEBUG) Log.w(TAG, construirTexto(texto.toString(), 5)); }
+    public static void w(Object texto) {
+        String cadena = construirTexto(texto.toString(), 5);
+        if (DEBUG) Log.w(TAG, cadena);
+        if (SAVE_FILE) saveFichero(TIPO.WARNING, cadena);
+    }
 
     /**
      * Mensaje de INFORMATION
      *
      * @param texto Texto a mostrar
      */
-    public static void i (Object texto){ if (DEBUG) Log.i(TAG, construirTexto(texto.toString(), 5)); }
+    public static void i(Object texto) {
+        String cadena = construirTexto(texto.toString(), 5);
+        if (DEBUG) Log.i(TAG, cadena);
+        if (SAVE_FILE) saveFichero(TIPO.INFORMATION, cadena);
+    }
 
     /**
      * Mensaje de VERBOSE
      *
      * @param texto Texto a mostrar
      */
-    public static void v (Object texto){ if (DEBUG) Log.v(TAG, construirTexto(texto.toString(), 5)); }
+    public static void v(Object texto) {
+        String cadena = construirTexto(texto.toString(), 5);
+        if (DEBUG) Log.v(TAG, cadena);
+        if (SAVE_FILE) saveFichero(TIPO.VERBOSE, cadena);
+    }
 
     /**
      * Muestra de forma tabulada un objeto JSON
      *
      * @param jsonObject Objeto JSON
      */
-    public static void json (JSONObject jsonObject){
+    public static void json(JSONObject jsonObject) {
         if (!DEBUG) return;
         try {
             l();
-            Log.d(TAG, dameInfoTrace(4) + jsonObject.toString(TABULACION_JSON));
+            String cadena = dameInfoTrace(4) + jsonObject.toString(TABULACION_JSON);
+            fw(context, new InfoLog(cadena));
+            Log.d(TAG, cadena);
+
             l();
         } catch (Exception ex) {
             e(ex.getMessage());
@@ -108,31 +177,43 @@ public class MyLog {
      *
      * @param jsonArray
      */
-    public static void json (JSONArray jsonArray){
+    public static void json(JSONArray jsonArray) {
         if (!DEBUG) return;
         try {
             l();
-
             Log.d(TAG, dameInfoTrace(4) + jsonArray.toString(TABULACION_JSON));
             l();
         } catch (Exception ex) {
             e(ex.getMessage());
         }
-
     }
 
     //Generamos la estructura del texto que concatenamos antes para saber de donde estamos mostrando el error
-    private static String construirTexto (String texto, int trace){
+    private static String construirTexto(String texto, int trace) {
         String cadena = dameInfoTrace(5);
         cadena += ": " + texto;
         return cadena;
     }
 
-    private static String dameInfoTrace (int elementoStackTrace){
+    private static void saveFichero(TIPO tipo, String texto) {
+        if (context != null) {
+            long yourmilliseconds = System.currentTimeMillis();
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String tiempo = sdf.format(new Date(yourmilliseconds));
+
+            texto = tipo + " " + texto;
+            fw(context, new InfoLog(texto));
+        }
+    }
+
+    private static String dameInfoTrace(int elementoStackTrace) {
 
         StackTraceElement[] st = Thread.currentThread().getStackTrace();
         String cadena = "";
         StackTraceElement stackTraceElement = st[elementoStackTrace];
+
+        traceElement = stackTraceElement;
+
         if (mostrarFichero) cadena += stackTraceElement.getFileName() + ", ";
         if (mostrarClase) {
             String[] partes = stackTraceElement.getClassName().split("\\.");
@@ -149,7 +230,7 @@ public class MyLog {
      *
      * @param p Puntos de ejecución a retroceder
      */
-    public static void trace (int p){
+    public static void trace(int p) {
         StackTraceElement[] st = Thread.currentThread().getStackTrace();
         for (int i = 0; i < (p); i++) {
             StackTraceElement stackTraceElement = st[i];
@@ -170,7 +251,7 @@ public class MyLog {
      *
      * @param p Indice de traza a mostrar
      */
-    public static String getTrace (int p){
+    public static String getTrace(int p) {
 
         StackTraceElement[] st = Thread.currentThread().getStackTrace();
         StackTraceElement stackTraceElement = st[p];
@@ -188,7 +269,7 @@ public class MyLog {
     }
 
 
-    private static String repeat (String texto, Integer n){
+    private static String repeat(String texto, Integer n) {
         StringBuilder buf = new StringBuilder(texto.length() * n);
         while (n-- > 0) {
             buf.append(texto);
@@ -205,17 +286,26 @@ public class MyLog {
      *
      * @param texto Texto a mostrar
      */
-    public static void f (String texto){
+    public static void f(String texto) {
 
         int longitud = 5;
         int trace = 6;
 
-        if (DEBUG)
-            Log.d(TAG, construirTexto("┌" + repeat("-", texto.length() + (longitud * 2)) + "┐", trace));
-        if (DEBUG)
-            Log.v(TAG, construirTexto("|" + repeat("*", 3) + "  " + texto + "  " + repeat("*", 3) + "|", trace));
-        if (DEBUG)
-            Log.d(TAG, construirTexto("└" + repeat("-", texto.length() + (longitud * 2)) + "┘", trace));
+        String linea1 = construirTexto("┌" + repeat("-", texto.length() + (longitud * 2)) + "┐", trace);
+        String linea2 =construirTexto("|" + repeat("*", 3) + "  " + texto + "  " + repeat("*", 3) + "|", trace);
+        String linea3 = construirTexto("└" + repeat("-", texto.length() + (longitud * 2)) + "┘", trace);
+
+        if (DEBUG) {
+            Log.d(TAG, linea1);
+            Log.v(TAG, linea2 );
+            Log.d(TAG, linea3);
+        }
+
+        if (SAVE_FILE) {
+            saveFichero(TIPO.VERBOSE, linea1);
+            saveFichero(TIPO.VERBOSE, linea2);
+            saveFichero(TIPO.VERBOSE, linea3);
+        }
     }
 
     /**
@@ -226,18 +316,27 @@ public class MyLog {
      *
      * @param texto Texto a mostrar
      */
-    public static void c (String texto){
+    public static void c(String texto) {
 
         int longitud = 5;
-
         int trace = 6;
 
-        if (DEBUG)
-            Log.d(TAG, construirTexto("╔" + repeat("═", texto.length() + (longitud * 2)) + "╗", trace));
-        if (DEBUG)
-            Log.v(TAG, construirTexto("║" + repeat(" ", 5) + texto + repeat(" ", 5) + "║", trace));
-        if (DEBUG)
-            Log.d(TAG, construirTexto("╚" + repeat("═", texto.length() + (longitud * 2)) + "╝", trace));
+
+        String linea1 = construirTexto("╔" + repeat("═", texto.length() + (longitud * 2)) + "╗", trace);
+        String linea2 = construirTexto("║" + repeat(" ", 5) + texto + repeat(" ", 5) + "║", trace);
+        String linea3 = construirTexto("╚" + repeat("═", texto.length() + (longitud * 2)) + "╝", trace);
+
+        if (DEBUG) {
+            Log.d(TAG, linea1);
+            Log.v(TAG, linea2 );
+            Log.d(TAG, linea3);
+        }
+
+        if (SAVE_FILE) {
+            saveFichero(TIPO.VERBOSE, linea1);
+            saveFichero(TIPO.VERBOSE, linea2);
+            saveFichero(TIPO.VERBOSE, linea3);
+        }
 
     }
 
@@ -250,30 +349,70 @@ public class MyLog {
      *
      * @param texto Texto a mostrar
      */
-    public static void ce (String texto){
+    public static void ce(String texto) {
 
         int longitud = 5;
-
         int trace = 6;
 
-        if (DEBUG)
-            Log.e(TAG, construirTexto("╔" + repeat("═", texto.length() + (longitud * 2)) + "╗", trace));
-        if (DEBUG)
-            Log.e(TAG, construirTexto("║" + repeat(" ", 5) + texto + repeat(" ", 5) + "║", trace));
-        if (DEBUG)
-            Log.e(TAG, construirTexto("╚" + repeat("═", texto.length() + (longitud * 2)) + "╝", trace));
 
+
+
+        String linea1 = construirTexto("╔" + repeat("═", texto.length() + (longitud * 2)) + "╗", trace);
+        String linea2 = construirTexto("║" + repeat(" ", 5) + texto + repeat(" ", 5) + "║", trace);
+        String linea3 = construirTexto("╚" + repeat("═", texto.length() + (longitud * 2)) + "╝", trace);
+
+        if (DEBUG) {
+            Log.d(TAG, linea1);
+            Log.v(TAG, linea2 );
+            Log.d(TAG, linea3);
+        }
+
+        if (SAVE_FILE) {
+            saveFichero(TIPO.ERROR, linea1);
+            saveFichero(TIPO.ERROR, linea2);
+            saveFichero(TIPO.ERROR, linea3);
+        }
     }
-
 
 
     /**
      * Dibuja una linea de separacion de log
      * ej: ----------------------------------------------------------------------------------------------------
      */
-    public static void l (){
+    public static void l() {
         i(repeat("-", 100));
     }
+
+
+    /**
+     * Escribe el log en un fichero de texto
+     *
+     * @param context
+     * @param info
+     */
+    private static void fw(Context context, InfoLog info) {
+
+        if (!SAVE_FILE) return; //si no esta habilitado el escribir en el fichero salimos del metodo
+
+        try {
+            String carpetaAlmacenamiento = context.getExternalFilesDir(CARPETA_LOG).getPath();
+
+            //comprobamos si existe el directorio donde se almacenara el log, si no es asi lo creamos.
+            File f = new File(carpetaAlmacenamiento);
+            if (!f.isDirectory()) {
+                f.mkdirs();
+            }
+
+            FileOutputStream fos = new FileOutputStream(carpetaAlmacenamiento + "/log.txt", true);
+            //FileOutputStream fos = context.openFileOutput(carpetaAlmacenamiento +"/log.txt", Context.MODE_APPEND);
+            OutputStreamWriter fout = new OutputStreamWriter(fos);
+            fout.write(info.toLog() + "\n");
+            fout.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
 
 }
 
